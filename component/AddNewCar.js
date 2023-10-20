62b9071401c32340064db4fea397f5dad539a401
Mohammed Umar <umar@40bears.com>
2023-10-20 15:19:03 +0530

    update home and add new car page

---
+++ component/AddNewCar.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Footer from './footer'
+
+const AddNewCar = () => {
+  return (
+  <View>
+    <View>
+      <Text>AddNewCar</Text>
+    </View>
+    <Footer/>
+    </View>
+  )
+}
+
+export default AddNewCar
+
+const styles = StyleSheet.create({})
